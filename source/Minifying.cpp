3d28ae5d4f2588ff7f857945fa2600bad09f2a27
AmrAmin761 <85031098+AmrAmin761@users.noreply.github.com>
2021-12-19 19:04:56 +0200

    Delete source directory

--- source/Minifying.cpp
+++
@@ -1,8 +0,0 @@
-#include "Minifying.h"
-
-
-void Minify(string* XML_file) {
-    XML_file->erase(remove(XML_file->begin(), XML_file->end(), '\n'), XML_file->end());
-    XML_file->erase(remove(XML_file->begin(), XML_file->end(), ' '), XML_file->end());
-
-}
